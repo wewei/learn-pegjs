4c2e2971784cd3027fde4de9d6c95294ff020c9e
Wei Wei <lyweiwei@outlook.com>
2020-06-23 12:14:49 +0800

    Build DPD

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,83 @@ import { generate } from 'pegjs';
 import path from 'path';
 import fs from 'fs-extra';
 
+interface CellDef {
+  name: string;
+  trigger: Block[];
+  reference: Block[];
+  output: Block[];
+}
+
+interface Block {
+  type: BlockType;
+  name: string;
+}
+
+type NodeType = BlockType | 'Cell';
+type BlockType = 'Data' | 'Event';
+type Predicate<T> = (value: T, index: number, obj: T[]) => boolean;
+
+const isHaving = <T>(pred: Predicate<T>) => (arr: T[]) =>
+  arr.findIndex(pred) >= 0;
+
+const isEvent = (block: Block) => block.type === 'Event';
+
+function createDataPipeline(cellDefs: CellDef[]) {
+  const typeMap = new Map<string, NodeType>();
+  const cells: string[] = [];
+  const triggerMap = new Map<string, string[]>();
+  const referenceMap = new Map<string, string[]>();
+  const outputMap = new Map<string, string[]>();
+
+  const isConflicted = ({ name, type }: Block) =>
+    typeMap.has(name) && typeMap.get(name) !== type;
+
+  function processBlocks(blocks: Block[]): string[] {
+    if (isHaving(isConflicted)(blocks)) {
+      throw new Error('Conflicted block');
+    }
+    blocks.forEach(({ type, name }) => typeMap.set(name, type));
+    return blocks.map((b) => b.name);
+  }
+
+  function processTriggers(blocks: Block[]): string[] {
+    if (blocks.length === 0) {
+      throw new Error('At least one trigger is required');
+    }
+    if (blocks.length > 1 && isHaving(isEvent)(blocks)) {
+      throw new Error('Trigger conflict');
+    }
+    return processBlocks(blocks);
+  }
+
+  function processReference(blocks: Block[]): string[] {
+    return processBlocks(blocks);
+  }
+
+  function processOutput(blocks: Block[]): string[] {
+    return processBlocks(blocks);
+  }
+
+  cellDefs.forEach(({ name, trigger, reference, output }) => {
+    if (typeMap.has(name)) {
+      throw new Error(
+        typeMap.get(name) === 'Cell' ? 'Duplicated cells' : 'Type conflict'
+      );
+    }
+    typeMap.set(name, 'Cell');
+    cells.push(name);
+    triggerMap.set(name, processTriggers(trigger));
+    referenceMap.set(name, processReference(reference));
+    outputMap.set(name, processOutput(reference));
+  });
+
+  return { typeMap, cells, triggerMap, referenceMap, outputMap };
+}
+
 Promise.all([
   fs.readFile(path.resolve(__dirname, '../syntax/DPD.pegjs'), 'utf8'),
-  fs.readFile(path.resolve(__dirname, '../test/test.dpd'), 'utf8'),
+  fs.readFile(path.resolve(__dirname, '../test/collab-editor.dpd'), 'utf8'),
 ])
-  .then(([syntax, text]) => generate(syntax).parse(text))
-  .then((obj) => JSON.stringify(obj, null, 2))
+  .then(([syntax, text]) => generate(syntax).parse(text) as CellDef[])
+  .then(createDataPipeline)
   .then(console.log);
--- syntax/DPD.pegjs
+++ syntax/DPD.pegjs
@@ -2,45 +2,24 @@ Start
   = cells: (Cell _)* { return cells.map(arr => arr[0]); }
 
 Cell
-  = name: Name _ ':' _ def: Translator
-  { return { name, type: 'Translator', def }; }
-  / name: Name _ ':' _ def: Reducer
-  { return { name, type: 'Reducer', def }; }
-  / name: Name _ ':' _ def: Composer
-  { return { name, type: 'Composer', def }; }
+  = name: Name _ ':' _ trigger: BlockList _ '=>' _ reference: BlockList _ '=>' _ output: BlockList
+  { return { name, trigger, reference, output }; }
+  / name: Name _ ':' _ trigger: BlockList _ '=>' _ output: BlockList
+  { return { name, trigger, reference:[], output }; }
 
-Translator
-  = input:Message _ '#' _ refs:StateList _ '=>' _ output:Message
-  { return { input, refs, output }; }
-  / input:Message _ '=>' _ output:Message
-  { return { input, refs: [], output }; }
+BlockList
+  = block: Block
+  { return [block]; }
+  / '(' _ ')'
+  { return []; }
+  / '(' _ head: Block _ tail: (',' _ Block _)* ')'
+  { return [head, ...tail.map(arr => arr[2])]; }
 
-
-Reducer
-  = input:Message _ '#' _ refs:StateList _ '=>' _ output:State
-  { return { input, refs, output }; }
-  / input:Message _ '=>' _ output:State
-  { return { input, refs: [], output }; }
-
-Composer
-  = input: StateList _ '=>' _ output: State
-  { return { input, output }; }
-
-StateList
-  = '[' _ names:NameList _ ']'
-  { return names; }
-
-State
-  = '[' _ name:Name _ ']'
-  { return name; }
-
-Message
-  = '<' _ name:Name _ '>'
-  { return name; }
-
-NameList
-  = head:Name tail: (_ ',' _ Name)*
-  { return [head, ...tail.map(arr => arr[3])]; }
+Block
+  = name: Name
+  { return { type: 'Data', name }; }
+  / '@' name: Name
+  { return { type: 'Event', name}; }
 
 Name
   = $ ([_a-zA-Z][_a-zA-Z0-9]*)
